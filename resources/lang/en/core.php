16adafb981af9f6263b33af21666a1bc2cfac07d
youssef97hesham <48891247+youssef97hesham@users.noreply.github.com>
2021-03-08 05:49:50 +0200

    lang

--- resources/lang/en/core.php
+++ resources/lang/en/core.php
@@ -24,6 +24,77 @@ return [
     "ARABIC" => "ARABIC",
     "ENGLISH" => "ENGLISH",
 
+    "H-sld-t1" => "Best Solutions for",
+    "H-sld-t2" => "Car Security",
+    "H-sld-t3" => " Services",
+    "H-sld-d1" => " Our Devices provide a full solution to keep your vehicle safe all the time.",
+    "H-sld-d2" => "By Protecting you and all types of vehicles from theft.",
+    "H-sld-b" => "Buy Now",
+
+
+    "H-sld-c1-t" => "Flat Rate Fees",
+    "H-sld-c1-d" => "It is a long established fact that and reader will by the readable based.",
+    "H-sld-c2-t" => "24/7 Tracking",
+    "H-sld-c2-d" => "Stay secured around the clock by tracking your vehicle anytime.",
+    "H-sld-c3-t" => "Easy Tools",
+    "H-sld-c3-d" => "Track your car using our easy to use apps.",
+
+
+    "H-abt-t" => "About Us",
+    "H-abt-d1" => "We're leading ",
+    "H-abt-d2" => "Vehicle ",
+    "H-abt-d3" => "Tracking Service",
+    "H-abt-d4" => " in Egypt",
+    "H-abt-d5" => "We successively delivered high quality vehicle tracking devices working in Egypt.",
+    "H-abt-p1" => "More than 10 years of experience in vehicle security devices.",
+    "H-abt-p2" => "We always provide flexible & smart solutions. ",
+    "H-abt-p3" => "Unique latest machinary used the Logistics project.",
+    "H-abt-btm1" => "Year of Success",
+    "H-abt-btm2" => "Since we established in 2010 experience &amp; still a growing protfolio day by day!",
+    "H-fts-t" => "FEATURES",
+    "H-fts-d1" => "Our Devices Come with Various ",
+    "H-fts-d2" => "Outstanding Features",
+
+
+    "H-fts-c1-t" => "Location",
+    "H-fts-c1-d" => "This feature helps you to determine the location of your vehicle.",
+
+    "H-fts-c2-t" => "MIC",
+    "H-fts-c2-d" => "Our devices come with a Mic to hear all what's going on in the car",
+
+    "H-fts-c3-t" => "START & STOP",
+    "H-fts-c3-d" => "This is a service that give you the capability to Stop and start the car from your mobile phone.",
+
+    "H-fts-c4-t" => "GEOGRAPHICAL",
+    "H-fts-c4-d" => "This feature let's you determine the geographical area of the vehicle",
+
+    "H-fts-c5-t" => "SPEED",
+    "H-fts-c5-d" => "This is a service that let's you determine the vehicle speed",
+
+    "H-fts-c6-t" => "BATTERY",
+    "H-fts-c6-d" => "Give an alert on your phone if the car battery is stolen",
+
+    "H-fts-c7-t" => "INTERNAL BATTERY",
+    "H-fts-c7-d" => "The device is equipped with an internal battery that works in the event that the car battery is disconnected",
+
+    "H-fts-c8-t" => "FUEL CONSUMPTION",
+    "H-fts-c8-d" => "Reduce fuel and consumption by monitoring the route",
+
+    "H-fts-c9-t" => "SAFETY",
+    "H-fts-c9-d" => "The system works on the user name and password system",
+
+    "H-prd-t" => "Our Products",
+    "H-prd-d1" => "We provide",
+    "H-prd-d2" => " the best services",
+    "H-prd-d3" => "and installation",
+    "H-prd-d4" => "within 24 hours ",
+
+    "H-app-t" => "Download Our Tracking App",
+    "H-aap-d" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore aliqua. Lorem ipsum dolor sit amet, eiusmod tempor dolore aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.",
+
+    "H-faq-t" => "Helpful FAQ's",
+    "H-faq-d1" => "Frequently Asked ",
+    "H-faq-d2" => "Questions",
 
 
 ];
